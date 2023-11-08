54f0d24b1d97a8e154f21321425bf6273a99c7f3
Nik_MD <nik.kalyagin@gmail.com>
2023-11-08 16:12:47 +0500

    1.3

--- src/app/Background.js
+++ src/app/Background.js
@@ -3,7 +3,7 @@ import './styles.css';
 
 const Background = () => {
   return (
-    <video autoPlay muted loop className="background">
+    <video autoPlay muted loop className="background" poster="/phon.png">
       <source src="/firstScreen.mp4" type="video/mp4" />
       
     </video>
